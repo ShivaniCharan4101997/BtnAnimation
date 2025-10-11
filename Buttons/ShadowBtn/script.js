261afd565114f2e21fa2941cd97e932951cbc4b2
ShivaniCharan4101997 <charanshivani8@gmail.com>
2025-10-11 18:49:06 +0530

    added comment

--- Buttons/ShadowBtn/script.js
+++ Buttons/ShadowBtn/script.js
@@ -6,17 +6,22 @@ button.addEventListener("mousemove", (e) => {
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
 
+  // e.clientX and e.clientY are the mouse’s coordinates relative to the viewport.
+  // Subtracting rect.left and rect.top gives you coordinates relative to the button’s top-left corner.
+  // → So (x, y) tells you where inside the button the mouse currently is.
   const centerX = rect.width / 2;
   const centerY = rect.height / 2;
-
+  // The midpoint of the button (used to figure out how far away the cursor is from the center).finding the btns center point
   const offsetX = x - centerX;
   const offsetY = y - centerY;
+  // These tell you how far (and in what direction) the mouse is from the center.If offsetX is positive → cursor is on the right.If offsetY is positive → cursor is below the center.
 
   const shadowX = offsetX / 5;
   const shadowY = offsetY / 3;
 
   const insetX = offsetX / 20;
   const insetY = offsetY / 8;
+  // shadowX/shadowY control outer shadows,while insetX/insetY control inner shadows (lighting inside the button).
 
   button.style.boxShadow = `
     inset ${-insetX}px ${-insetY}px 6px var(--white),
